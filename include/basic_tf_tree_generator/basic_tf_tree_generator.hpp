54d45d0ef8878be382a854f064baa2f926bc4455
perezsaura-david <perezsaura.david@gmail.com>
2021-11-25 15:51:44 +0100

    Documentation

--- include/basic_tf_tree_generator/basic_tf_tree_generator.hpp
+++ include/basic_tf_tree_generator/basic_tf_tree_generator.hpp
@@ -18,21 +18,35 @@
 #include "nav_msgs/msg/odometry.hpp"
 
 class BasicTFTreeGenerator:public as2::Node{
-    public:
-        BasicTFTreeGenerator();
+  public:
+    /**
+     * @brief Constructor of the BasicTFTreeGenerator object
+     * 
+     */
+    BasicTFTreeGenerator();
 
-        void setupTf();
-        void publishTFs();
+    /**
+     * @brief Initial setup for node Transforms
+     * This function generate the Transform tree
+     * from Map to Baselink.
+     */
+    void setupTf();
 
-    private:
-        std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-        std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tfstatic_broadcaster_;
-        
-        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-        void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    /**
+     * @brief Publish Transforms tree
+     * 
+     */
+    void publishTFs();
 
-        std::vector<geometry_msgs::msg::TransformStamped> tf2_fix_transforms_;
-        geometry_msgs::msg::TransformStamped odom2base_link_tf_;
+  private:
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tfstatic_broadcaster_;
+    
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+    std::vector<geometry_msgs::msg::TransformStamped> tf2_fix_transforms_;
+    geometry_msgs::msg::TransformStamped odom2base_link_tf_;
 };
 
 #endif 
