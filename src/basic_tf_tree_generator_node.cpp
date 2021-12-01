3e1a8c47bd42a06cdf0acf756c9943df6371b835
Miguel <miferco97@gmail.com>
2021-12-01 16:34:32 +0100

    new as2::spinLoop() implemented

--- src/basic_tf_tree_generator_node.cpp
+++ src/basic_tf_tree_generator_node.cpp
@@ -1,11 +1,15 @@
 // "Copyright [year] <Copyright Owner>"
 
 #include "basic_tf_tree_generator.hpp"
+#include "as2_core/core_functions.hpp"
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<BasicTFTreeGenerator>());
+  auto node = std::make_shared<BasicTFTreeGenerator>();
+  //TODO: check if freq is needed
+  as2::spinLoop(node);
+
   rclcpp::shutdown();
   return 0;
 }
