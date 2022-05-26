328f93532540d2772d5e7ab141dbf800a21fc632
Pedro Arias <pedroariasperez96@gmail.com>
2022-05-26 18:22:02 +0200

    Removed name from launch

--- launch/basic_tf_tree_generator_launch.py
+++ launch/basic_tf_tree_generator_launch.py
@@ -9,7 +9,6 @@ def generate_launch_description():
         Node(
             package='basic_tf_tree_generator',
             executable='basic_tf_tree_generator_node',
-            name='basic_tf_tree_generator',
             namespace=LaunchConfiguration('drone_id'),
             output='screen',
             emulate_tty=True
