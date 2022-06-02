b3db6e244e320c4a3725b6b8fd90cb8e00ed5a2c
Pedro Arias <pedroariasperez96@gmail.com>
2022-06-02 17:02:34 +0200

    get default drone_id value from environment

--- launch/basic_tf_tree_generator_launch.py
+++ launch/basic_tf_tree_generator_launch.py
@@ -1,11 +1,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, EnvironmentVariable
 
 def generate_launch_description():
     return LaunchDescription([
-        DeclareLaunchArgument('drone_id', default_value='drone0'),
+        DeclareLaunchArgument('drone_id', default_value=EnvironmentVariable('AEROSTACK2_SIMULATION_DRONE_ID')),
         Node(
             package='basic_tf_tree_generator',
             executable='basic_tf_tree_generator_node',
